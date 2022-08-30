b610318f22133c1e844cb090acb53dc89f3fafd5
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-30 12:37:56 +0300

    add js close modal

--- js/modal.js
+++ js/modal.js
@@ -4,10 +4,40 @@ const refs = {
   modal: document.querySelector("[data-modal]"),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener("click", openModal);
+refs.closeModalBtn.addEventListener("click", closeModal);
+
+function windowModalRemoveKeydownClick() {
+  window.removeEventListener("keydown", closeModalEscape);
+  refs.modal.removeEventListener("click", closeModalBackdrop);
+}
 
 function toggleModal() {
   document.body.classList.toggle("modal-open");
   refs.modal.classList.toggle("backdrop__is-hidden");
+}
+
+function openModal() {
+  window.addEventListener("keydown", closeModalEscape);
+  toggleModal();
+  refs.modal.addEventListener("click", closeModalBackdrop);
+}
+
+function closeModal() {
+  windowModalRemoveKeydownClick();
+  toggleModal();
+}
+
+function closeModalEscape(e) {
+  if (e.code === "Escape") {
+    toggleModal();
+    windowModalRemoveKeydownClick();
+  }
+}
+
+function closeModalBackdrop(e) {
+  if (e.target === e.currentTarget) {
+    windowModalRemoveKeydownClick();
+    toggleModal();
+  }
 }
