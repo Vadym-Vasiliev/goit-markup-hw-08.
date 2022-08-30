3c3cefcce95c2dfa1dd468bcfa87a44c2dfb8a51
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-30 10:13:37 +0300

    add footer form

--- js/form-footer.js
+++ js/form-footer.js
@@ -1,13 +1,28 @@
 let form = document.querySelector(".footer-subscribe__form");
-let formInput = document.querySelectorAll(".footer-subscribe__input");
+let formInput = document.querySelector(".footer-subscribe__input");
+
+function validateMail(mail) {
+  let symbol =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return symbol.test(String(mail).toLowerCase());
+}
 
 form.onsubmit = function () {
   let inputValue = formInput.value;
 
   if (inputValue === "") {
-    formInput.classList.add("error!");
+    console.log("Поле не заповнено!");
+    formInput.classList.add("error");
     return false;
   } else {
-    formInput.classList.remove("error!");
+    formInput.classList.remove("error");
+  }
+
+  if (!validateMail(inputValue)) {
+    console.log("Поле заповнено не вірно!");
+    formInput.classList.add("error");
+    return false;
+  } else {
+    formInput.classList.remove("error");
   }
 };
